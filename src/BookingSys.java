4b02a40f2db4f7dae2984f7211d8a035ec11d994
MOhamed Abdallah <119700010+MO-87@users.noreply.github.com>
2024-12-12 15:39:27 +0200

    Update BookingSys.java

--- src/BookingSys.java
+++ src/BookingSys.java
@@ -1,24 +1,24 @@
 import java.util.concurrent.locks.ReentrantLock;
 
 public class BookingSys {
-    private final boolean[][] seats; // made it boolean to represent the seat availability...
-    private final ReentrantLock lock; // will be used to ensure thread safety, in other words to prevent STARVATION!!
+    private final boolean[][] seats;
+    private final ReentrantLock lock;
 
     public BookingSys(int rows, int cols) {
-        this.seats = new boolean[rows][cols];
-        this.lock = new ReentrantLock();
+        seats = new boolean[rows][cols];
+        lock = new ReentrantLock();
     }
 
     public boolean bookSeat(int row, int col) {
-        this.lock.lock(); // locking to ensure thread safety
+        lock.lock();
         try {
-            if (!this.seats[row][col]) {
-                this.seats[row][col] = true;
+            if (!seats[row][col]) {
+                seats[row][col] = true;
                 return true;
             }
             return false;
         } finally {
-            this.lock.unlock(); // releasing the lock, this step is so important
+            lock.unlock();
         }
     }
 }
